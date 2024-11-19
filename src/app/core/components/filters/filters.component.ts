0fa4ab8fd28d78ad9b463f8ab61d1c52b1880405
Antonio Marín <amrodriguezjarote@gmail.com>
2024-11-19 16:14:22 +0100

    Add reset button into filters

--- src/app/core/components/filters/filters.component.ts
+++ src/app/core/components/filters/filters.component.ts
@@ -3,18 +3,19 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { changeFilters } from '../../../state/actions/data.actions';
 import { selectFilters } from '../../../state/selectors/data.selector';
-import { map } from 'rxjs';
-import { FILTER_TYPE_IN_OPTION } from '../../../data/filters';
+import { map, Observable } from 'rxjs';
+import { FILTER_TYPE_IN_OPTION, INITIAL_FILTERS } from '../../../data/filters';
 import { FormToggleComponent } from '../form/form-toggle/form-toggle.component';
 import { SearchSVGComponent } from '../../svg/seach-svg/seach-svg.component';
 import { showModal } from '../../../state/actions/context.actions';
 import { FormRowComponent } from '../form/form-row/form-row.component';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-filters',
   standalone: true,
-  imports: [ReactiveFormsModule, FormToggleComponent, SearchSVGComponent, FormRowComponent, SearchSVGComponent],
+  imports: [ReactiveFormsModule, FormToggleComponent, SearchSVGComponent, FormRowComponent, SearchSVGComponent, CommonModule],
   templateUrl: './filters.component.html',
   styleUrl: './filters.component.css'
 })
@@ -23,13 +24,14 @@ export class FiltersComponent implements OnInit{
   filters: FormGroup = new FormGroup({});
   options = FILTER_TYPE_IN_OPTION;
 
-  constructor(private readonly store:Store){}
+  constructor(private readonly store:Store){
+    this.filters.addControl('pending', new FormControl(false))
+  }
 
   ngOnInit(): void {
       this.store.select(selectFilters)
-      .pipe(
-        map(filters => this.filters.addControl('pending', new FormControl(filters.pending)))
-      ).subscribe()
+      .pipe(map(filters => this.filters.patchValue({pending:filters.pending})))
+      .subscribe()
   }
 
   changeToggle(){
@@ -39,5 +41,14 @@ export class FiltersComponent implements OnInit{
 
   showSearchModal(){
     this.store.dispatch(showModal({ modalType:'search-record'}));
+  }
+
+  resetFilters(){
+    this.store.dispatch(changeFilters({filters:INITIAL_FILTERS}))
+  }
+
+  showResetFilters():Observable<boolean>{
+    return this.store.select(selectFilters)
+    .pipe( map(filters => JSON.stringify(filters) !== JSON.stringify(INITIAL_FILTERS)));
   }
 }
